df5b8c9a780630fae94b351f17f551a9c926b762
Henrique Goffeau <henriquegarnier15@gmail.com>
2025-01-21 10:04:28 -0300

    correção biblioteca

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgre753@10.36.113.40:5432/todo'
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
